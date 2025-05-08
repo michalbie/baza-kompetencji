22a24ea2044d02a521d0bcebc4c25398015376c3
Michał Biernacki <userofficeit@gmail.com>
2025-05-08 18:54:18 +0200

    Added routing and modified prettier config file

---
+++ frontend/src/components/router/Router.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../../pages/Home/Home";
+import { NotFound } from "../../pages/NotFound/NotFound";
+import { Login } from "../../pages/Login/Login";
+import ProtectedRoute from "./ProtectedRoute";
+
+const AppRouter = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<ProtectedRoute>
+							<Home />
+						</ProtectedRoute>
+					}
+				/>
+				<Route path="/login" element={<Login />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default AppRouter;
